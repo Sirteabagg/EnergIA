432e1a53bc0cbed86421674519b476ac289990d1
ethanECE <ethan.petain@gmail.com>
2025-11-16 00:54:17 +0100

    add : error table add : procedures

---
+++ db/procedure.sql
@@ -0,0 +1,43 @@
+
+DROP PROCEDURE IF EXISTS get_average_consumption_by_building;
+DELIMITER $$
+
+CREATE PROCEDURE get_average_consumption_by_building(IN date_start DATE, IN date_end DATE)
+BEGIN
+    SELECT building_id, AVG(value) AS average_consumption
+    FROM consommations
+    WHERE timestamp BETWEEN date_start AND date_end
+    GROUP BY building_id;
+END;
+
+DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS get_error_outliers_measure_by_sensor;
+DELIMITER $$
+CREATE PROCEDURE get_error_outliers_measure_by_sensor()
+BEGIN
+    SELECT
+        s.name,
+        e.temperature,
+        e.humidity,
+        e.power_consumption
+       
+    FROM Error e JOIN Sensor s ON e.sensor_id = s.sensor_id;
+    
+END $$
+
+DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS get_buildings_with_overconsumption;
+DELIMITER $$
+CREATE PROCEDURE get_buildings_with_overconsumption()
+BEGIN
+    SELECT b.name
+    FROM Measure m JOIN Building b ON m.building_id = b.building_id
+    GROUP BY m.building_id
+    HAVING AVG(power_consumption) > (SELECT AVG(power_consumption) FROM Measure);
+END;$$
+
+DELIMITER 
--- db/schema.sql
+++ db/schema.sql
@@ -20,3 +20,14 @@ CREATE TABLE Measure (
     FOREIGN KEY (sensor_id) REFERENCES Sensor(sensor_id)
 );
 
+CREATE TABLE Error (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    timestamp VARCHAR(25),
+    temperature FLOAT,
+    humidity FLOAT,
+    power_consumption FLOAT,
+    building_id INT,
+    sensor_id INT,
+    FOREIGN KEY (building_id) REFERENCES Building(building_id),
+    FOREIGN KEY (sensor_id) REFERENCES Sensor(sensor_id)
+);
